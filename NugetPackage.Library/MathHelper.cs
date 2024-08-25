04140879ca445042719274b33d30aa65987c2fc8
“Ervis <ervistrupja@outlook.com>
2024-08-25 20:47:31 +0200

    added more methods

--- NugetPackage.Library/MathHelper.cs
+++ NugetPackage.Library/MathHelper.cs
@@ -3,5 +3,23 @@
     public class MathHelper
     {
         public int Add(int a, int b) => a + b;
+
+        public int Subtract(int a, int b) => a - b;
+
+        public int Multiply(int a, int b) => a * b;
+
+        public int Divide(int a, int b) => a / b;
+
+        public int Modulus(int a, int b) => a % b;
+
+        public int Power(int a, int b) => (int)Math.Pow(a, b);
+
+        public int Factorial(int a)
+        {
+            if (a == 0)
+                return 1;
+            else
+                return a * Factorial(a - 1);
+        }
     }
 }
