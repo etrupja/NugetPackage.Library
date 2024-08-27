de05c21162fffd07bd5809ef28fe917a5fd5e96c
Ervis Trupja <ervistrupja@outlook.com>
2024-08-27 21:36:07 +0200

    0204 clip code

--- NugetPackage.Library/MathHelper.cs
+++ NugetPackage.Library/MathHelper.cs
@@ -6,6 +6,9 @@ namespace NugetPackage.Library
     {
         public int Factorial(int a)
         {
+            if (a < 0)
+                return -1;
+
             if (a > 10)
                 return 0;
 
