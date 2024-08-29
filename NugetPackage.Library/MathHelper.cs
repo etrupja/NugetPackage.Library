92651008f9b879a7000aa9677bf6bd6a793baab3
“Ervis <ervistrupja@outlook.com>
2024-08-29 14:02:13 +0200

    added AddV2 method

--- NugetPackage.Library/MathHelper.cs
+++ NugetPackage.Library/MathHelper.cs
@@ -3,6 +3,7 @@
     public class MathHelper
     {
         public int Add(int a, int b) => a + b;
+        public int AddBy2(int a, int b) => a + b + 2;
 
         public int Factorial(int a)
         {
